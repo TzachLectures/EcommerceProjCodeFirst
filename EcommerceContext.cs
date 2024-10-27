b4ba5c57563ae414910d571b1c3b0ac134920def
Benny Dabush <bennydabush@gmail.com>
2024-10-27 21:39:16 +0200

    Add project files.

---
+++ EcommerceContext.cs
@@ -0,0 +1,25 @@
+﻿using EcommerceProjCodeFirst.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EcommerceProjCodeFirst
+{
+    internal class EcommerceContext :DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderItem> OrderItems { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=LAPTOP-M1H6FNPI\MSSQLSERVER02;Database=EcommerceProject;Trusted_Connection=True;TrustServerCertificate=True;");
+        }
+
+    }
+}
